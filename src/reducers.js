03fff893fc58dfa72597cbb44cdce78e12fa86de
Allen Anderson <dande313@gmail.com>
2017-11-10 15:45:56 +0000

    automatically backed up by learn

--- src/reducers.js
+++ src/reducers.js
@@ -25,7 +25,7 @@ export function recomendedBooks(state = [], action){
 export function combineReducers(reducers){
   return (state = {}, action) => {
     return Object.keys(reducers).reduce(
-      (nextState, key)=>{
+      (nextState, key, reducers)=>{
         nextState[key] = reducers[key](state[key], action);
         return nextState
       }, {}
