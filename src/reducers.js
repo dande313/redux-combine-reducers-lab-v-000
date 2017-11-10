0390e9959563eb0a6713ece5b490e64eaedddabe
Allen Anderson <dande313@gmail.com>
2017-11-10 15:49:29 +0000

    automatically backed up by learn

--- src/reducers.js
+++ src/reducers.js
@@ -1,25 +1,24 @@
 export function books(state = [], action){
   switch (action.type) {
-  case "ADD_BOOK":
-    return [].concat(state, action.payload)
-  case "REMOVE_BOOK":
-    let idx = state.indexOf(action.payload)
-    return [].concat(state.slice(0, idx), state.slice(idx + 1, state.length))
-  default:
-    return state
+    case 'ADD_BOOK':
+      return [].concat(state, action.payload)
+    case 'REMOVE_BOOK':
+      let idx = state.indexOf(action.payload)
+      return [].concat(state.slice(0, idx), state.slice(idx + 1, state.length))
+    default:
+      return state
   }
 }
 
-
-export function recommendedBooks(state = [], action){
+export function recomendedBooks(state = [], action){
   switch (action.type) {
-  case "ADD_RECOMMENDED_BOOK":
-    return [].concat(state, action.payload)
-  case "REMOVE_RECOMMENDED_BOOK":
-    let idx = state.indexOf(action.payload)
-    return [].concat(state.slice(0, idx), state.slice(idx + 1, state.length))
-  default:
-    return state
+    case 'ADD_RECOMMENDED_BOOK':
+      return [].concat(state, action.payload)
+    case 'REMOVE_RECOMMENDED_BOOK':
+      let idx = state.indexOf(action.payload)
+      return [].concat(state.slice(0, idx), state.slice(idx + 1, state.length))
+    default:
+      return state
   }
 }
 
